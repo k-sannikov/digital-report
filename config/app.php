beba893efa4d85601edf058185840ffea55e2fd7
Kirill Sannikov <kir.sannikow@mail.ru>
2019-06-25 13:42:27 +0300

    Русификация проекта

--- config/app.php
+++ config/app.php
@@ -80,7 +80,7 @@ return [
     |
     */
 
-    'locale' => 'en',
+    'locale' => 'ru',
 
     /*
     |--------------------------------------------------------------------------
@@ -174,6 +174,10 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        /*
+         * Own Service Providers...
+         */
+        App\Providers\DigitalReportServiceProvider::class,
 
     ],
 
@@ -225,6 +229,11 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        /*
+         * Own Class Aliases...
+         */
+        'DigitalReport' => App\Facades\DigitalReportFacade::class,
+
 
     ],
 
